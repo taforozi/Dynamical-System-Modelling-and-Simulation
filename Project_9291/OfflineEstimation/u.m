5b1545ee721875678a43514a2cd30d5c0a84936c
taforozi <81076999+taforozi@users.noreply.github.com>
2021-04-04 17:35:27 +0300

    Delete Project_9291 directory

--- Project_9291/OfflineEstimation/u.m
+++
@@ -1,5 +0,0 @@
-function u_out = u(t)
-
- u_out = sin(t) + 0.1*cos(2*t) +  1.5*sin(3*t) + 2*cos(4*t) + 2.5*sin(5*t) + 3*cos(6*t);
-
-end
--- Project_9291/OfflineEstimation/u_validation.m
+++
@@ -1,5 +0,0 @@
-function u_out = u_validation(t)
-
- u_out = sin(3.4*t) + 0.1*cos(2.6*t) + 0.5*sin(3.2*t) + cos(2.2*t) + 0.9*sin(5.4*t) + 5*cos(8*t);
-
-end
